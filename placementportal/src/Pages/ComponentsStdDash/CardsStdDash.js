2d2ea3a65ac95718eac6af5fb525ccd1bec38dea
shashwatsrii <96522015+shashwatsrii@users.noreply.github.com>
2022-07-25 02:58:25 +0530

    css errors

--- placementportal/src/Pages/ComponentsStdDash/CardsStdDash.js
+++ placementportal/src/Pages/ComponentsStdDash/CardsStdDash.js
@@ -10,12 +10,12 @@ function Card(cmpName,cmpDes,Domain,logoCmp) {
                 <Link className='LinkJobDescription' to="/StudentDashboard/JobDescription">
                 <div className="Card">
                     <div className="CompanyLogoFeed"><img className='CmpLogo' src={lgoCmop} /></div>
-                    <div className="CompanyDetails">
+                    <div className="CompanyDetailsCard">
                         <div className='CompanyName'>Amazon</div>
-                        <div className='CompanyDes'>NO 1 Comapany in the world Gives you a lot of experriense </div>
+                        <div className='CompanyDes'>Leading Corporate in the world offering you success </div>
                     </div>
                     <div className="CardrightSide">
-                        <div className='domainOfJob'>Tech/Non Tech</div>
+                        <div className='domainOfJob'>Technical</div>
                         <div className='ApplyBtn'><Link className='ApplyNowText' to='/StudentDashboard/JobApplication'>Apply Now</Link></div>
                     </div>
                 </div>
